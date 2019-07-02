274b9664556a1e72e6d5ce673692a26643f7eb82
Clement1987 <jaminclement1987@gmail.com>
2019-07-02 12:23:32 +0200

    ajout menu navigation sur l'index.php

---
+++ site/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href=".css">
+	<title>Mon super site</title>
+</head>
+<body>
+	<header>
+		<h1>Site title</h1>
+		<img src="#" alt=""/>
+		<nav>
+			<li><a href="#">Acceuil</a></li>
+			<li><a href="#">Nusery</a></li>
+			<li><a href="#">Activity</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</nav>
+	</header>
+	<footer>
+	</footer>
+</body>
+</html>

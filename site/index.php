4366ccf7e7b204aedfbada5dd9efd130baddf62a
dierickd <dierickd@outlook.fr>
2019-07-02 18:57:09 +0200

    mise à jour des fichier et ajout de la connection sql + récupération des data

--- site/index.php
+++ site/index.php
@@ -1,23 +1,42 @@
+<?php
+try {
+	$pdo = new PDO('mysql:host=localhost;dbname=daycare;charset=utf8', 'root', '');	
+} catch (PDOException $e) {
+	die('Impossible de se connecter à la base de donnée');
+}
+
+$req = $pdo->query('SELECT * FROM chat');
+$message = $req->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href=".css">
-	<title>Mon super site</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<title>Take care of him</title>
+	<link rel="stylesheet" href="public/css/app.css">
 </head>
 <body>
-	<header>
-		<h1>Site title</h1>
-		<img src="#" alt=""/>
-		<nav>
-			<li><a href="#">Acceuil</a></li>
-			<li><a href="#">Nusery</a></li>
-			<li><a href="#">Activity</a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Contact</a></li>
-		</nav>
-	</header>
-	<footer>
-	</footer>
+	
+	<div class="container">
+	<?php include('template/header.php'); ?>
+
+	<article class="main_article">
+		<?php foreach ($message as $m) { ?>
+		
+		<div class="jumbotron">
+			<p><?= $m['message'] ?> </p>
+		</div>
+
+		<?php } ?>
+		<p></p>
+	</article>
+
+	<?php include('template/footer.php'); ?>
+	</div>
 </body>
 </html>

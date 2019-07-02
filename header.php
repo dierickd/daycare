cd5e4f2017f829c6d96468690738c8b2dd8d4ed3
clement <48488113+Clement1987@users.noreply.github.com>
2019-07-02 17:50:39 +0200

    add image + index + footer +header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+        <!-- name's site + banner + text -->
+        <header id="top">
+			<div class="header_disposition">
+					<div class="main_title"><h1>My Baby's protection</h1></div>
+					<img src="/public/img/banner2.jpg" alt="child picture laughing in banner"/>
+					<div class="banner_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae laboriosam, temporibus obcaecati modi cumque ratione suscipit quod reprehenderit earum nisi consectetur hic, sit eius iusto quibusdam, esse nostrum deserunt adipisci.</div>
+			</div>
+			<!-- logo -->
+			<div class="logo_container">
+				<img src="https://via.placeholder.com/50" alt="site logo">
+			</div>
+			<!-- navigation menu -->
+			<nav>
+				<li><a href="#top">home</a></li>
+				<li><a href="#">Nursery</a></li>
+				<li><a href="#">Activity</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</nav>
+		</header>

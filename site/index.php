b65213e22190db0608788518a1924dae346f5460
dierickd <dierickd@outlook.fr>
2019-07-05 16:12:13 +0200

    modif

--- site/index.php
+++ site/index.php
@@ -17,22 +17,20 @@ $message = $req->fetchAll(PDO::FETCH_ASSOC);
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Take care of him</title>
-	<link rel="stylesheet" href="public/css/app.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="public/css/app.css">
 </head>
 
 <body>
 	
-	<div class="container">
 	<?php include('template/header.php'); ?>
+	<div class="container-fluid">
 
 	<article class="main_article">
 		<?php foreach ($message as $m) { ?>
 		
 		<div class="jumbotron">
+			<p><?= $m['date'] ?> </p>
 			<p><?= $m['message'] ?> </p>
 		</div>
 
@@ -42,5 +40,9 @@ $message = $req->fetchAll(PDO::FETCH_ASSOC);
 
 	<?php include('template/footer.php'); ?>
 	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="public/js/app.js"></script>
 </body>
 </html>
--- site/template/header.php
+++ site/template/header.php
@@ -1,24 +1,26 @@
+
 <!-- name's site + banner + text -->
 <header id="top">
     <div class="header">
-            <div class="main_title"><h1>My Baby's protection</h1></div>
-            <img src="public/img/banner.jpg" alt="child picture laughing in banner"/>
-            <div class="banner_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae laboriosam, temporibus obcaecati modi cumque ratione suscipit quod reprehenderit earum nisi consectetur hic, sit eius iusto quibusdam, esse nostrum deserunt adipisci.</div>
+        <h1>My Baby's protection</h1>
+        <div class="banner_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae laboriosam, temporibus obcaecati modi cumque ratione suscipit quod reprehenderit earum nisi consectetur hic, sit eius iusto quibusdam, esse nostrum deserunt adipisci.</div>
     </div>
-    <!-- navigation menu -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"><img src="https://via.placeholder.com/50" alt="site logo"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
+</header>
+<!-- navigation menu -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" data-toggle="sticky-onscroll">
+    <a class="navbar-brand" href="#">
+        <img src="https://via.placeholder.com/50" alt="site logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
             <a class="nav-item nav-link active" href="#">Home</a>
             <a class="nav-item nav-link" href="#">Nusre</a>
             <a class="nav-item nav-link" href="#">Activity</a>
             <a class="nav-item nav-link" href="#">About</a>
             <a class="nav-item nav-link" href="#">Contact</a>
-            </div>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>

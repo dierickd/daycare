c4b9f6edab61b00fd82b5f1bce5809004552fbeb
Clement1987 <jaminclement1987@gmail.com>
2019-07-10 22:54:04 +0200

    ajout page about / css et image

--- site/views/pages/about.php
+++ site/views/pages/about.php
@@ -1,10 +1,48 @@
 
 <?php include('../layout/header_nav.php'); ?>
-
-<div class="jumbotron">
-    <h1>Vous êtes sur la page about</h1>
-    <a href="home.php">Home</a>
+<div class="aboutContainer">
+	<div class="card-group">
+  	<div class="card">
+    	<img src="../../public/img/Daisy.jpg" class="card-img-top" alt="Daisy Picture">
+    	<div class="card-body">
+      	<h5 class="card-title text-center font-weight-bold">Daisy</h5>
+      	<p class="card-text text-justify">8 years working in leisure centres, former state nurse in the private hospital "Roche-Beauvois". Wanting to get closer to the children, she decided to do her reconversion. For 8 years in our centre, with a calm and tender temperament, she has the necessary qualities to keep your children's attention! </p>
+      	<p class="card-text text-center"><small class="text-muted">diploma :
+      		<ul class="diplome">
+      			<li>DEI obtained</li>
+      			<li>first aid gesture</li>
+      		</ul></small></p>
+    	</div>
+  	</div>
+  	<div class="card">
+    	<img src="../../public/img/Pascaline.jpg" class="card-img-top" alt="Pascaline Picture">
+    	<div class="card-body">
+      	<h5 class="card-title text-center font-weight-bold">Pascaline</h5>
+      	<p class="card-text text-center"><span>director of the centre<span><br/><br/>
+      		30 years of experience in childcare, expert in organizing events for children to entertain them safely.</p>
+      	<p class="card-text text-center"><small class="text-muted">diploma :
+      		<ul class="diplome">
+      			<li>DEI obtained</li>
+      			<li>first aid gesture</li>
+      		</ul></small></p>
+    	</div>
+  	</div>
+  	<div class="card">
+    	<img src="../../public/img/Denzel.jpg" class="card-img-top" alt="Denzel Picture">
+    	<div class="card-body">
+      	<h5 class="card-title text-center font-weight-bold">Denzel</h5>
+      	<p class="card-text texte-justify">5 years of experience in leisure centres, accompanying disabled children in amusement parks. Very good social skills with the most difficult children.</p>
+      	<p class="card-text text-center"><small class="text-muted">diploma :
+      		<ul class="diplome">
+      			<li>BAFA obtained</li>
+      			<li>first aid gesture</li>
+      		</ul></small></p>
+    	</div>
+	</div>
+	</div>
 </div>
-
+<div class="iframe-container">
+    <iframe class="googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2691.7843772412775!2d1.3038486156293045!3d47.571983579182394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e357c77c100ab7%3A0x30f1b097f8b37b6e!2sLyc%C3%A9e+des+M%C3%A9tiers+de+L&#39;+H%C3%B4tellerie+et+du+Tourisme+du+Val+de+Loire!5e0!3m2!1sfr!2sfr!4v1562788865243!5m2!1sfr!2sfr" width="290" height="200" frameborder="1" style="border:1" allowfullscreen></iframe>
+</div>
 
 <?php include('../layout/footer.php'); ?>
